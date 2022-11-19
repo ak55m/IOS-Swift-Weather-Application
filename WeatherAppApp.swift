7635808c63ff6a50ccd82ea6a4140443166ebe6a
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2022-11-19 00:31:05 -0600

    initial

---
+++ WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Akeem Mohammed on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
