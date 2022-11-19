73020eed90fb5d37303638b0c83625043a5dc72a
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2022-11-19 00:23:17 -0600

    initial

---
+++ Managers/LocationManager.swift
@@ -0,0 +1,41 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Akeem Mohammed on 11/16/22.
+//
+
+import Foundation
+import CoreLocation // this is used to get the users current location
+
+// this class manage everyting about location without running into error
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init(){
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+        
+}
+
+
